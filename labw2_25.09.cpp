bbfa76639c8a15be9b32af60d4a3b569dc8378f9
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-09-25 15:01:28 +0300

    add task 1.14

---
+++ labw2_25.09.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n % 2 == 0) {
+        printf("true\n");
+    }
+    else {
+        printf("false\n");
+    }
+    if (n % 10 != 0) {
+        printf("false\n");
+    }
+    else {
+        printf("true\n");
+    }
+
+    int x, y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    if (x - y >= 6) {
+        printf("true\n");
+    }
+    else {
+        printf("false\n");
+    }
+
+}
