836ef98d9c6e72b9a2a39b45d9a4186174561e4f
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-09-28 14:38:57 +0300

    Add commit task 1_2.1

---
+++ labwork1_28.09.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int n;
+    double x;
+    printf("n: ");
+    scanf("%d", &n);
+    printf("x: ");
+    scanf("%lf\n", &x);
+    for (int i = 0; i < n; i++) {
+        x = sin(x);
+    }
+    printf("answer: %lf\n", x);
+}
