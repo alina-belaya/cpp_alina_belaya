a69559289c1b1ad9c8fcebe3262da67111e9fed3
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-09-28 16:13:04 +0300

    Add commit task 2.2d 28.09

---
+++ labwork3_28.09.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int x;
+    int n;
+    printf("n: ");
+    scanf("%d", &n);
+    printf("x: ");
+    scanf("%d", &x);
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += pow(x, i*i);
+    }
+    printf("answer: %d\n", sum);
+}
