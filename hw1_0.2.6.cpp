ce4b94392c5083cb3a723028d374dfeeb97795c2
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-10-04 20:08:17 +0300

    Add commit homework1 task 0.2.6

---
+++ hw1_0.2.6.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    double a, b, c, r;
+
+    printf("Angle degree 1: ");
+    scanf("%lf", &a);
+
+    printf("Angle degree 2: ");
+    scanf("%lf", &b);
+
+    c = 180 - (a + b);
+
+    printf("Radius: ");
+    scanf("%lf", &r);
+
+    double s = 2 * r * r * sin(a) * sin(b) * sin(c);
+    printf("Answer: %lf\n", s);
+}
