16b5b56bdb7ee155a294c05d7d0ba89a162a2b2e
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-10-04 19:52:48 +0300

    Add commit homework1 task 0.0.10

---
+++ hw1_0.0.10.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    double a, t, v;
+
+    printf("a:");
+    scanf("%lf", &a);
+
+    printf("t:");
+    scanf("%lf", &t);
+
+    printf("v:");
+    scanf("%lf", &v);
+
+    double s = (a * t * t) / 2;
+    double t1 = v / a;
+
+    printf("Path after t seconds: %lf\nTime when will be speed V: %lf\n", s, t1);
+}
