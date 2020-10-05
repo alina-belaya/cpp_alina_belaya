821acb240649608b8428c5ec15a141d0d8de4bfc
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-10-05 15:33:04 +0300

    Add commit task1_2.5

---
+++ cw1_2.5_05.10.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int factorial(int i)
+{
+	if (i == 0) return 1;
+	else return i * factorial(i - 1);
+}
+
+int main()
+{
+	int n, i, res;
+
+	printf("n = ");
+	scanf("%d", &n);
+	printf("n! = %d\n", factorial(n));
+}
