5dac4c513df4f6569fad755a87c99f40205ac46c
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-10-05 14:32:26 +0300

    Add commit task1_2.3

---
+++ cw1_2.3_05.10.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+int main()
+{
+    int n;
+
+    printf("N: ");
+    scanf("%d", &n);
+    printf("%d! = ", n);
+    int i = 1;
+    printf("%d", i);
+    while (i<n) {
+        i++;
+        printf("*%d", i);
+    }
+}
