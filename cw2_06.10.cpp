67f64cbdd3e5935618cd0753b97a1b1f82e6f728
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-10-06 13:46:45 +0300

    Add commit task1_2.47

---
+++ cw2_06.10.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int n;
+
+    printf("n = ");
+    scanf("%d", &n);
+    int x = n * 2;
+
+    for (int i = 2; i <= x; i++) {
+        int k = 0;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                k++;
+                break;
+            }
+        }
+        if (k == 0) { printf("%d ", i); }
+    }
+}
