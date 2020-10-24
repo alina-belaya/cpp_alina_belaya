41d0c70eaa149472744153cd88ddc9619ecd7013
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-10-24 19:58:52 +0300

    Add commit homework3 task 19е.1_1

---
+++ hw.task.19e_1.1.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+double isReLu(int a, int x) {
+    double res = x / (sqrt(1 + a * x * x));
+    if (x >= 0) { return x; }
+    else { return res; }
+}
+
+int main()
+{
+    int a, x;
+
+    printf("a x = ");
+    scanf("%d %d", &a, &x);
+
+    double ans = isReLu(a, x);
+    printf("%lf\n", ans);
+}
