ef9d231ed7f1cbb6314dee1ec2786395149ff4bb
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-10-24 20:00:21 +0300

    Add commit homework4 task 19а.1_2

---
+++ hw.task.19a_1.2.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+double my_power(double x, int k)
+{
+    double X = x;
+    for (int i = 1; i < k; i++)
+        X *= x;
+    return X;
+}
+
+int main()
+{
+    int k;
+    double x;
+
+    printf("k = ");
+    scanf("%d", &k);
+
+    printf("x = ");
+    scanf("%lf", &x);
+
+    double ans = my_power(x, k)/k;
+
+    printf("ans: %lf\n", ans);
+}
