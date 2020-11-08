b6616aece562c6cccb10675b62010b2c8fd6bafd
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-11-08 19:48:45 +0200

    Add commit homework5 task 22.1_2

---
+++ hw.task.22_1.2.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int summCheck(int n, int x) {
+    int summ = 0;
+    do {
+        summ += (x % 10);
+        x = x / 10;
+    } while (x > 0);
+    if (summ == n) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main()
+{
+    int n, count = 0;
+
+    printf("Input n: ");
+    scanf("%d", &n);
+
+    for (int i = 100; i <= 999; i++) {
+        if (summCheck(n, i)) { 
+            count++;
+        }
+    }
+
+    printf("Result: %d numbers\n", count);
+}
