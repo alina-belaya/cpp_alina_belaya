d650fa4bdd9ec350dca1380eaf1af499eb123a19
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-11-08 19:52:10 +0200

    Add commit homework5 task 35в.1_2

---
+++ hw.task.35v_1.2.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int x1 = -99, x2 = -99, x3 = -99, xn, count = 0;
+	
+	do {
+		xn = x3 + x1 + 100;
+		x3 = xn;
+		x1 = x2;
+		count++;
+	} while (xn <= 0);
+	
+	printf("Answer: x[%d] = %d > 0\n", count+3, xn);
+}
