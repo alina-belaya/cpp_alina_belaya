613e5605ce95cabf1d46af8e37ee14576e361de4
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-11-22 19:04:19 +0200

    Add commit homework #8 task 2 9e

---
+++ hw8_9e.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<cmath>
+
+bool Prim(int n) {
+    for (int d = 2; d <= n / 2; d++)
+        if (n % d == 0)
+            return false;
+    return true;
+}
+
+int main()
+{
+    int n;
+    printf("n = ");
+    scanf("%d", &n);
+
+    int* arr = new int[n];
+    int i;
+    int pr = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("a[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        if (Prim(arr[i]))
+            pr++;
+    }
+    printf("how many prime numbers: %d\n", pr);
+}
