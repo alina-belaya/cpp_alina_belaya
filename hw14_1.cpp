c397569f498007b359e55c28d02426609d18df11
alina-belaya <71881465+alina-belaya@users.noreply.github.com>
2020-12-09 16:48:21 +0200

    Add commit homework #14 1в

---
+++ hw14_1.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+typedef
+struct Date {
+    unsigned int day;
+    unsigned int mon;
+    int year;
+} Date;
+
+int input(Date* x) {
+    Date res;
+    printf("day: ");
+    scanf("%u", &res.day);
+
+    printf("mon: ");
+    scanf("%u", &res.mon);
+
+    printf("year: ");
+    scanf("%d", &res.year);
+
+    *x = res;
+    return 0;
+}
+
+void output(const Date x) {
+    printf("day.month.year = %u.%u.%d\n", x.day, x.mon, x.year);
+}
+
+int main() {
+    Date x;
+    input(&x);
+    output(x);
+}
